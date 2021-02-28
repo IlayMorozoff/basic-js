603351f02d4cf4ac9306a35fc2911127551f168f
IlayMorozoff <morozoff1994@yandex.ru>
2021-02-28 03:17:22 +0300

    feat: add new files for tests and add solution for count-cats and honoi-tower

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+
+  let b =[];
+
+  let c = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let k = 0; k < matrix[i].length; k++) {
+      b.push(matrix[i][k]);
+    }
+  } 
+
+  let e = b.filter((item) => item === '^^');
+  
+  if(e) {
+    return e.length;
+  } else {
+    return 0;
+  }
+
 };
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  
+  const turnsSpeedsec = turnsSpeed / 3600;
+
+  const turns = (2 ** disksNumber) - 1;
+
+  const seconds = Math.floor(turns / turnsSpeedsec);
+  
+  return {
+    turns,
+    seconds,
+  }
 };
