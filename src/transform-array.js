8373af72d79d494667080fc551c6db178c9c7b65
IlayMorozoff <morozoff1994@yandex.ru>
2021-03-03 20:40:53 +0300

    feat: add solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error("argument's function is not array");
+  } else if (arr.length === 0) {
+    return [];
+  }
+
+  let convertedArray = [];
+
+  for ( let i = 0; i < arr.length; i++ ) {
+
+    if(arr[i] !== '--discard-next' && arr[i] !== '--discard-prev' && arr[i] !== '--double-next' && arr[i] !== '--double-prev') {
+      convertedArray.push(arr[i]);
+
+    } else if (arr[i] == '--discard-next' && i !== arr.length - 1) {
+      i++;
+
+    } else if (arr[i] == '--double-next' && i !== arr.length - 1) {
+      convertedArray.push(arr[i + 1]);
+    } else if (arr[i] == '--double-prev' && i !== 0 && arr[i - 2] !== '--discard-next') {
+      convertedArray.push(arr[i - 1]);
+    } else if (arr[i] == '--discard-prev' && i !== 0 && arr[i - 2] !== '--discard-next') {
+      convertedArray.pop(arr[i - 1]);
+    }
+}
+
+
+  return convertedArray;
+  
 };
